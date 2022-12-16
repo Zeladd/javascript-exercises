21ea1350ee83bbc3ed212092f3249fcc2df86d33
Zeladd <filippesteam@gmail.com>
2022-12-16 23:20:13 +0200

    Finish 05.sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum = 0;
+    if(typeof(min) !== "number" || typeof(max) !== "number") {
+        return "ERROR";
+    }
+    if(min > max) {
+        let swap = min;
+        min = max;
+        max = swap;
+    }
+    if(min < 0) {
+        return "ERROR";
+    }
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
