eaea4acea05b053a6310310b5639ffae354a0072
Zeladd <filippesteam@gmail.com>
2022-12-16 23:05:59 +0200

    Finish 04.removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for (let element in elements) {
+        if (!array.includes(elements[element])) continue;
+        array.splice(array.indexOf(elements[element]), 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
